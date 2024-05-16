51ac46213da8d4e18e8fd7527bbe56ad5e5b17ee
Antonis Anastasiou <antonisanastasiou2012@gmail.com>
2024-05-16 21:50:33 +0300

    added the base of the application
    Added the mysql-connector-j-8.4.0 library. Added the first frame of the project.

---
+++ ArenaPassApp/ArenaPass/src/LoginUI.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class LoginUI {
+    private JTextField UsernameField;
+    private JPasswordField PasswordField;
+    private JButton LoginButton;
+    private JButton RegisterButton;
+    private JButton ForgotPasswordButton;
+}
