5c250b9c521b5db081b0286d6d1c9e4af43338f7
Antonis <antonisanastasiou2012@gmail.com>
2024-05-20 20:17:17 +0300

    added BuySeasonTicket form

---
+++ ArenaPassApp/ArenaPass/src/BuySeasonTicket.java
@@ -0,0 +1,81 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class BuySeasonTicket extends JFrame{
+    private JPanel BuySeasonTicketForm;
+    private JComboBox mainMenuDropDown;
+    private JButton logoutBtn;
+    private JTextField textField1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+
+    public BuySeasonTicket(){
+        setupFrame();
+        setUpActions();
+    }
+
+    private void setupFrame(){
+        dispose();
+        add(BuySeasonTicketForm);
+        setTitle("Buy Tickets");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(1920,1080);
+    }
+
+    private void setUpActions() {
+        mainMenuDropDown.addActionListener(this::switchPanel);
+        logoutBtn.addActionListener(this::logout);
+    }
+
+    private void switchPanel(ActionEvent actionEvent) {
+        JOptionPane.showMessageDialog(mainMenuDropDown, mainMenuDropDown.getSelectedItem());
+
+        switch (mainMenuDropDown.getSelectedIndex()){
+            case 0:
+                setVisible(false);
+                dispose();
+                new MainPage().setVisible(true);
+                break;
+            case 1:
+                setVisible(false);
+                dispose();
+                new BuyTicket().setVisible(true);
+                break;
+            case 2:
+                // new BuySeasonTicket().setVisible(true);
+                break;
+            case 3:
+                // cancel reservation
+                break;
+            case 4:
+                // ticket history
+                break;
+            case 5:
+                // Leaderboards
+                break;
+            case 6:
+                // Busses
+                break;
+            case 7:
+                // Feedback
+                break;
+            case 8:
+                // contact us
+                break;
+        }
+    }
+
+    private void logout(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        new LoginUI().setVisible(true);
+    }
+}
+
+
