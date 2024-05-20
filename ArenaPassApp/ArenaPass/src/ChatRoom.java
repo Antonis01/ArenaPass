12fbef29f5e6439f62dbfa4b59c7cd94d6f0614e
valantis <valantisano@hotmail.com>
2024-05-20 20:23:40 +0300

    ChatRoom

---
+++ ArenaPassApp/ArenaPass/src/ChatRoom.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ChatRoom {
+    private JPanel panel1;
+    private JComboBox mainMenuDropDown;
+    private JTextField textField1;
+    private JButton logoutBtn;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton button1;
+}
