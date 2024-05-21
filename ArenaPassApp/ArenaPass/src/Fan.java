b56176d6df305238e6463e2267cf829db69cca53
lambrosp46 <38588773+lambrosp46@users.noreply.github.com>
2024-05-21 03:17:37 +0300

    fan fix

--- ArenaPassApp/ArenaPass/src/Fan.java
+++ ArenaPassApp/ArenaPass/src/Fan.java
@@ -1,9 +1,13 @@
+import java.util.Date;
+
 public class Fan extends Account{
     boolean isApproved;
-    public Fan(String firstName, String lastName, String phoneNumber, String homeAddress, String city, String ID, String emailAddres, String password, boolean isApproved) {
-        super(firstName, lastName, phoneNumber, homeAddress, city, ID, emailAddres, password);
-        this.isApproved=isApproved;
+
+    public Fan(String firstName, String lastName, String phoneNumber, String homeAddress, String city, String ID, String emailAddress, String password, Date birthDate, boolean isApproved) {
+        super(firstName, lastName, phoneNumber, homeAddress, city, ID, emailAddress, password, birthDate);
+        this.isApproved = isApproved;
     }
+
 
     public boolean isApproved() {
         return isApproved;
