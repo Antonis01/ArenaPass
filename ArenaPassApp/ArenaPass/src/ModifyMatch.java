dca1708bce10831150b17b0ecbc2035cee2fb764
valantis <valantisano@hotmail.com>
2024-05-21 19:41:05 +0300

    ModifyMatch Design

---
+++ ArenaPassApp/ArenaPass/src/ModifyMatch.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class ModifyMatch {
+    private JPanel panel1;
+    private JComboBox AdminMenuDropBox;
+    private JTextField textField1;
+    private JButton logoutBtn;
+    private JTextField textField2;
+    private JButton DateBtn;
+    private JTextField textField3;
+    private JButton TimeBtn;
+    private JTextField textField4;
+    private JButton ScoreBtn;
+    private JButton AwayStantBtn;
+    private JButton HomeStantBtn;
+    private JButton UpdateBtn;
+}
---
+++ ArenaPassApp/ArenaPass/src/SelectMatchModify.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class SelectMatchModify {
+    private JPanel panel1;
+    private JPanel SelectMatch;
+    private JComboBox AdminMenuDropBox;
+    private JTextField textField1;
+    private JButton logoutBtn;
+    private JButton ModifyBtn;
+}
