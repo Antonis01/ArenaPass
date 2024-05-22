3b58ecf74bf39c02404fa7d6a76f97d087be8e21
valantis <valantisano@hotmail.com>
2024-05-22 19:00:29 +0300

    ModifyMatch update

--- ArenaPassApp/ArenaPass/src/ModifyMatch.java
+++ ArenaPassApp/ArenaPass/src/ModifyMatch.java
@@ -1,9 +1,11 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 
 
 public class ModifyMatch extends JFrame{
     private JPanel ModifyMatch;
-    private JComboBox AdminMenuDropBox;
+    private JComboBox AdminMenuDropDown;
     private JTextField textField1;
     private JButton logoutBtn;
     private JTextField textField2;
@@ -12,13 +14,12 @@ public class ModifyMatch extends JFrame{
     private JButton TimeBtn;
     private JTextField textField4;
     private JButton ScoreBtn;
-    private JButton AwayStantBtn;
-    private JButton HomeStantBtn;
     private JButton UpdateBtn;
 
     public ModifyMatch(){
-
         setupFrame();
+        setUpActions();
+
     }
 
     private void setupFrame() {
@@ -28,5 +29,40 @@ public class ModifyMatch extends JFrame{
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+    private void setUpActions() {
+        AdminMenuDropDown.addActionListener(this::switchPanel);
+        logoutBtn.addActionListener(this::logout);
+    }
+
+    private void switchPanel(ActionEvent actionEvent) {
+        JOptionPane.showMessageDialog(AdminMenuDropDown, AdminMenuDropDown.getSelectedItem());
+
+        switch (AdminMenuDropDown.getSelectedIndex()){
+            case 0:
+                setVisible(false);
+                dispose();
+                new AdminMainPage().setVisible(true);
+                break;
+            case 1:
+                setVisible(false);
+                dispose();
+                new SelectMatchModify().setVisible(true);
+                break;
+            case 2:
+                setVisible(false);
+                dispose();
+                new ModifyMatch().setVisible(true);
+                break;
+
+        }
+    }
+
+    private void logout(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        new LoginUI().setVisible(true);
+    }
 
 }
+
+
--- ArenaPassApp/ArenaPass/src/SelectMatchModify.java
+++ ArenaPassApp/ArenaPass/src/SelectMatchModify.java
@@ -1,26 +1,64 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class SelectMatchModify extends JFrame{
     private JPanel panel1;
-    private JPanel SelectMatch;
-    private JComboBox AdminMenuDropBox;
+    private JPanel SelectMatchModifyForm;
+    private JComboBox AdminMenuDropDown;
     private JTextField textField1;
     private JButton logoutBtn;
     private JButton ModifyBtn;
 
 
     public SelectMatchModify(){
-
         setupFrame();
+        setUpActions();
     }
 
     private void setupFrame() {
-        add(SelectMatch);
-        setTitle("ArenaPass ModifyMatchPage");
+        add(SelectMatchModifyForm);
+        setTitle("ArenaPass SelectMatchModifyPage");
         setSize(1920, 1080);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
+    private void setUpActions() {
+        AdminMenuDropDown.addActionListener(this::switchPanel);
+        logoutBtn.addActionListener(this::logout);
+        ModifyBtn.addActionListener(this::modify);
+    }
 
+    private void switchPanel(ActionEvent actionEvent) {
+        JOptionPane.showMessageDialog(AdminMenuDropDown, AdminMenuDropDown.getSelectedItem());
+
+        switch (AdminMenuDropDown.getSelectedIndex()){
+            case 0:
+                setVisible(false);
+                dispose();
+                new AdminMainPage().setVisible(true);
+                break;
+            case 1:
+                setVisible(false);
+                dispose();
+                new SelectMatchModify().setVisible(true);
+                break;
+        }
+    }
+
+    private void logout(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        new LoginUI().setVisible(true);
+    }
+    private void modify(ActionEvent actionEvent) {
+        setVisible(false);
+        dispose();
+        new ModifyMatch().setVisible(true);
+    }
 }
+
+
+
+
+
