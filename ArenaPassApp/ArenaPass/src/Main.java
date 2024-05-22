b7b5f8f8b4e0a2c492f7cf08ba2f37e16003a213
lambrosp46 <38588773+lambrosp46@users.noreply.github.com>
2024-05-23 02:45:17 +0300

    buy ticket new

--- ArenaPassApp/ArenaPass/src/Main.java
+++ ArenaPassApp/ArenaPass/src/Main.java
@@ -3,8 +3,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
+
     public static void main(String[] args) throws SQLException {
-        new test().setVisible(true);
+        new BuyTicketNew().setVisible(true);
+        //new test().setVisible(true);
         new LoginUI().setVisible(true);
     }
 }
