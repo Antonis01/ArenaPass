d00ed1532bd1a4290cecffaccde58a86420368c6
Antonis <antonisanastasiou2012@gmail.com>
2024-05-23 19:54:53 +0300

    bug fix

--- ArenaPassApp/ArenaPass/src/LoginUI.java
+++ ArenaPassApp/ArenaPass/src/LoginUI.java
@@ -32,17 +32,23 @@ public class LoginUI extends JFrame {
 
     private void login(ActionEvent actionEvent) {
         if (ConnectDB.Connector(usernameField.getText(), new String(passwordField.getPassword()))) {
-            String role = ConnectDB.getRole(usernameField.getText(), new String(passwordField.getPassword()));
-            if(role.equals("admin")) {
-                JOptionPane.showMessageDialog(null, "Admin login successful!");
-                openAdminPage();
+            //String role = ConnectDB.getRole(usernameField.getText(), new String(passwordField.getPassword()));
+            //if(role.equals("admin")) {
+              //  JOptionPane.showMessageDialog(null, "Admin login successful!");
+                //openAdminPage();
+                //openAppAdminPage();
+            //}else{
+           // String role = ConnectDB.getRole(usernameField.getText(), new String(passwordField.getPassword()));
+           // if(role.equals("admin")) {
+               // JOptionPane.showMessageDialog(null, "Admin login successful!");
+            //    openAdminPage();
               //  openAppAdminPage();
-            }else{
-                JOptionPane.showMessageDialog(null, "Login successful!");
+            //}else{
+              //  JOptionPane.showMessageDialog(null, "Login successful!");
                 openMainPage();
-            }
-        } else {
-            JOptionPane.showMessageDialog(null, "Login failed!");
+            //}
+        //} else {
+          //  JOptionPane.showMessageDialog(null, "Login failed!");
         }
     }
 
