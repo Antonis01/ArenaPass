d6a5e1ad98aa7e13fe4197925eed8352ffeaa086
lambrosp46 <38588773+lambrosp46@users.noreply.github.com>
2024-05-29 02:03:14 +0300

    Created Busses form for admin

---
+++ ArenaPassApp/ArenaPass/src/AdminBussesForm.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class AdminBussesForm {
+    private JPanel thisPanel;
+    private JComboBox mainMenuDropDown;
+    private JLabel logo;
+    private JTextField textField1;
+    private JButton logoutBtn;
+    private JTable table1;
+    private JButton button1;
+}
